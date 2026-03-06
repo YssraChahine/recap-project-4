ef035f920fb586707e8673d1f8031b81c68b446b
Yssra Chahine <chahine@live.de>
2026-03-06 15:48:35 +0100

    Add color form and dynamic color creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColor) {
+    setColors([{ id: nanoid(), ...newColor }, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
+
+      <ColorForm onAddColor={handleAddColor} />
+
       <section className="color-grid">
-        {initialColors.map((color)=>(
-        <Color key={color.id} color={color} />
+        {colors.map((color) => (
+          <Color key={color.id} color={color} />
         ))}
       </section>
     </>
