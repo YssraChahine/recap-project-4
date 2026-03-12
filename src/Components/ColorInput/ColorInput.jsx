16e26661295a616a19fec29f029b20ef1e5c96e1
Yssra Chahine <chahine@live.de>
2026-03-12 09:38:08 +0100

    delete disabled

--- src/Components/ColorInput/ColorInput.jsx
+++ src/Components/ColorInput/ColorInput.jsx
@@ -1,14 +1,14 @@
 import "./ColorInput.css";
 
-export default function ColorInput({label,value,onChange,disabled = false,}) {
+export default function ColorInput({label,value,onChange}) {
   return (
     <div className="color-input-row">
 
       <label>{label}</label>
 
-      <input type="color" value={value} onChange={(event) => onChange(event.target.value)} disabled={disabled}/>
+      <input type="color" value={value} onChange={(event) => onChange(event.target.value)}/>
       
-      <input type="text" value={value} onChange={(event) => onChange(event.target.value)} disabled={disabled}/>
+      <input type="text" value={value} onChange={(event) => onChange(event.target.value)}/>
     
     </div>
   );
